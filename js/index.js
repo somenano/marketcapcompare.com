fc50eacf7379ac3417b194125603504f042135e6
Jason Pawlak <jasonapawlak@gmail.com>
2020-12-04 21:08:16 -0600

    various updates to user experience

--- js/index.js
+++ js/index.js
@@ -5,8 +5,11 @@ socket.emit('subscribe_all');
 socket.on('data', handle_data);
 socket.on('metadata', handle_metadata);
 
+window.data = undefined;
 window.metadata = undefined;
 window.default_selected_asset = 'NANO';
+window.default_selected_asset_id = 1567;
+window.first_load = true;
 
 function isAlpha(str) {
     if (!str) return false;
@@ -32,9 +35,9 @@ function get_selected_asset()
     const url_string = window.location.href;
     const url = new URL(url_string);
     const unsanitized_asset = url.searchParams.get("asset");
-    if (unsanitized_asset === null) return window.default_selected_asset;
+    if (unsanitized_asset === null && window.metadata['metadata'][window.default_selected_asset_id]) return window.default_selected_asset;
     const sanitized_asset = unsanitized_asset.replace(/[\W_]+/g,"");
-    if (sanitized_asset=='' || sanitized_asset === undefined) return window.default_selected_asset;
+    if ((sanitized_asset=='' || sanitized_asset === undefined) && window.metadata['metadata'][window.default_selected_asset_id]) return window.default_selected_asset;
     return sanitized_asset;
 }
 
@@ -44,14 +47,29 @@ function update_selected_asset(asset)
     const url_string = window.location.href;
     const url = new URL(url_string);
     url.searchParams.set('asset', asset);
-    self.location=url.href;
+    url.hash = '';
+    // self.location=url.href;
+    history.pushState('update_selected_asset', 'CompareMarketCap', url.href);
+    handle_data(window.data);
 }
+
+window.addEventListener('popstate', function (event) {
+    if (!window.first_load && window.data) handle_data(window.data);
+}, false);
 
 function handle_data(data)
 {
     // console.log(data);
+    window.data = data;
     update_dropdown(data);
     update_table(data);
+    if (window.first_load) {
+        const url = new URL(window.location.href);
+        if (url.hash.length > 0) {
+            self.location = window.location.href;
+        }
+        window.first_load = false;
+    }
 }
 
 function handle_metadata(metadata)
@@ -122,6 +140,7 @@ function update_table(data)
     let selected_asset_price;
     let selected_asset_metadata;
     let selected_asset_logo;
+    let selected_asset_rank;
     let counter = 0;
     for (let asset_data of data['data']) {
         if (asset_data['symbol'] == selected_asset){
@@ -132,6 +151,7 @@ function update_table(data)
             selected_asset_price = selected_asset_data['quote']['USD']['price'];
             selected_asset_metadata = window.metadata['metadata'][selected_asset_data['id']];
             selected_asset_logo = selected_asset_metadata['logo'];
+            selected_asset_rank = counter;
             update_description(selected_asset_data['id']);
             break;
         }
@@ -173,7 +193,7 @@ function update_table(data)
 <div class="p-2 text-right" style="width:300px">
 <div><span class="fs-1-5">`+selected_asset_name+` (`+selected_asset+`)</span></div>
 <div class="fs-1"><span>Price at #`+counter+`: $`+numberWithCommas(selected_asset_price_rel.toFixed(2))+` USD</span></div>
-<div class="fs-1"><span>Current Price: $`+numberWithCommas(selected_asset_price.toFixed(2))+` USD</span></div>
+<div class="fs-1"><span>Current price at #`+selected_asset_rank+`: $`+numberWithCommas(selected_asset_price.toFixed(2))+` USD</span></div>
 <div class="fs-0-7"><span>Difference: `+numberWithCommas((market_cap_diff*100).toFixed(0))+`%</span></div>
 </div>
 
@@ -184,23 +204,23 @@ function update_table(data)
 
         html += `
 
- <div class="pl-4 pr-4">&nbsp;</div>
+ <div class="pl-4 pr-4" id="`+counter+`">&nbsp;</div>
 
  <div class="p-2 text-center d-flex align-items-center">
   <div>
-  <div class="fs-3">`+counter+`</div>
+  <div class="fs-3"><a href="#`+counter+`" class="text-dark text-decoration-none">`+counter+`</a></div>
   <div class="fs-0-7">RANK</div>
   </div>
  </div>
 
  <div class="pl-4 pr-4">&nbsp;</div>
 
- <div class="p-2 d-flex align-items-center">
-  <img src="`+logo+`" width="32px">
+ <div class="p-2 d-flex align-items-center" id="`+symbol+`">
+  <a href="#`+symbol+`" class="text-dark text-decoration-none"><img src="`+logo+`" width="32px"></a>
  </div>
 
  <div class="p-2 text-left" style="width:300px">
-  <div><span class="fs-1-5 w-100">`+name+` (`+symbol+`)</span></div>
+  <div><span class="fs-1-5 w-100"><a href="#`+symbol+`" class="text-dark text-decoration-none">`+name+` (`+symbol+`)</a></span></div>
   <div class="fs-1"><span class="w-100">Market Cap: $`+numberWithCommas((market_cap/1000000000).toFixed(3))+` Billion USD</span></div>
   <div class="fs-1"><span class="w-100">Price: $`+numberWithCommas(price.toFixed(2))+` USD</span></div>
   <div class="fs-0-7"><span class="w-100">1hr: <span class="`+(price_change_1h > 0 ? 'text-success' : (price_change_1h < 0 ? 'text-danger' : 'text-dark'))+`">`+price_change_1h.toFixed(2)+`%</span> | 24hr: <span class="`+(price_change_24h > 0 ? 'text-success' : (price_change_24h < 0 ? 'text-danger' : 'text-dark'))+`">`+price_change_24h.toFixed(2)+`%</span> | 7d: <span class="`+(price_change_7d > 0 ? 'text-success' : (price_change_7d < 0 ? 'text-danger' : 'text-dark'))+`">`+price_change_7d.toFixed(2)+`%</span></span></div>
